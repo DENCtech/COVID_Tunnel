6e46cc0ef17b6c95eaa9ff4534f4682ce134d0d8
DENCtech <49325260+DENCtech@users.noreply.github.com>
2020-05-28 20:46:19 +0100

    Update COVID-19_Motion_sensor_project.ino

--- COVID-19_Motion_sensor_project/COVID-19_Motion_sensor_project.ino
+++ COVID-19_Motion_sensor_project/COVID-19_Motion_sensor_project.ino
@@ -12,23 +12,34 @@ int val2 = 0;                    // variable for reading the pin status
 const unsigned long interval = 5000;           // interval at which to blink (milliseconds)
 
 // Buttons and state
-int amButton = 11;
-int ssButton = 10;
+int amButton = 8;
+int ssButton = 12;
 int ambuttonState = LOW;
 int ssbuttonState = LOW;
+
 // Lights
 int relay2 = 5;
 int buzzer = 6;
+
 //variable resistors
 int var1 = A0;
 int var2 = A1;
+
+//ultrasonic sensor one
+const int trigPin3 = 13;    // Trigger
+const int echoPin3 = 12;    // Echo
+int distance3;
+float duration3;
+int state3;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(relay, OUTPUT);      // declare Relay as output
   pinMode(relay2, OUTPUT);
   pinMode(inputPin, INPUT);     // declare sensor as input
-  pinMode(inputPin2, INPUT);  
+  pinMode(inputPin2, INPUT); 
+  pinMode(amButton, INPUT);
+  pinMode(ssButton, INPUT); 
   pinMode(buzzer, OUTPUT);
   digitalWrite(relay, LOW);
   digitalWrite(relay2, LOW);
@@ -46,37 +57,44 @@ void loop() {
   lcd.print("ROBOTICS--CENTER");
   lcd.setCursor(0, 1);
   lcd.print("Mode: ");
-  lcd.setCursor(0, 2);
+  lcd.setCursor(-4, 2);
   lcd.print("ChemLevel: ");
-  lcd.setCursor(0, 3);
+  lcd.setCursor(-4, 3);
   lcd.print("Duration: ");
   ambuttonState = digitalRead(amButton);
   if(ambuttonState == HIGH){
+    lcd.setCursor(7, 1);
+    lcd.print("      ");
+    lcd.setCursor(7, 1);
+    lcd.print("AUTO");
     val = digitalRead(inputPin);  // read input value
     val2 = digitalRead(inputPin2);  // read input value
-    if (val == HIGH && val2 == LOW) {
+    if (val == HIGH) {
       digitalWrite(buzzer, HIGH);
+      delay(50);
+      digitalWrite(buzzer, LOW);
+      delay(50);
       digitalWrite(relay, HIGH);
       digitalWrite(relay2, HIGH);
-      Serial.println("Motion detected!");
       delay(5000);
-//      digitalWrite(buzzer, LOW);
-//      digitalWrite(relay, LOW);
-//      digitalWrite(relay2, LOW);
-//      Serial.println("Motion ended!");
-//      delay(100);
-    }
-    else{
-      if(val2 == HIGH && val == LOW){
-        digitalWrite(buzzer, LOW);
-        digitalWrite(relay, LOW);
-        digitalWrite(relay2, LOW);
-        Serial.println("Motion ended!");
-        delay(100);
+      digitalWrite(relay, LOW);
+      digitalWrite(relay2, LOW);
+      delay(10);
+      while(val2 == LOW){
+        val2 = digitalRead(inputPin2);// read input value
+        delay(10);
       }
+      digitalWrite(buzzer, HIGH);
+      delay(50);
+      digitalWrite(buzzer, LOW);
+      delay(50);
     }
   }
   else{
+    lcd.setCursor(7, 1);
+    lcd.print("      ");
+    lcd.setCursor(7, 1);
+    lcd.print("MANUAL");
     ssbuttonState = digitalRead(ssButton);
     if(ssbuttonState == HIGH){
       digitalWrite(buzzer, HIGH);
@@ -93,5 +111,16 @@ void loop() {
       delay(500);
     }
   }
-  
+}
+void getDistance3(){
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin3, LOW);
+
+  duration3 = pulseIn(echoPin3, HIGH);
+  distance3 = duration3 * 0.034 / 2;
+
+//  Serial.print("Distance3: ");
+//  Serial.println(distance3);
+
 }
